ab67216fc6c36b623d586427c0e1fc92798c1cb4
Eugene F <software.eugeneer@gmail.com>
2019-10-23 17:34:38 +0300

    Update test-scripts task

--- gulp/test-scripts.js
+++ gulp/test-scripts.js
@@ -3,13 +3,12 @@
 const gulp = require('gulp');
 const gulpEslint = require('gulp-eslint');
 
-module.exports = (callback) =>
-  gulp
-    .src([
-      '**/*.js',
-      '!**/*scsslint*',
-      '!nodex_modules/**/*'
-    ])
-    .pipe(gulpEslint())
-    .pipe(gulpEslint.format())
-    .pipe(gulpEslint.failAfterError());
+let counter = 0;
+module.exports = (callback) => {
+  let c = counter++;
+  console.log('RUN TEST SCRIPTS ' + c + ' STARTS');
+  setTimeout(function () {
+    console.log('RUN TEST SCRIPTS ' + c + ' DONE');
+    callback();
+  }, 1000);
+};
